5739e709b2ae3f2f7e7389185a1dd2f32fc74bd0
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2024-05-05 09:06:32 -0300

    fix: clear

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import RoutesApp from "./routes";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
-      <ToastContainer autoClose={1000}/>
       <RoutesApp />
     </div>
   );
